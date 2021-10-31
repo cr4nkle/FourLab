64c55050c8ac9eccc3ae060a4382d2e0657c69e9
Сергей Попович <cr4nkle@yandex.ru>
2021-10-31 21:57:05 +0800

    Изменение кода

--- FourLab/Form1.cs
+++ FourLab/Form1.cs
@@ -12,15 +12,58 @@ namespace FourLab
 {
     public partial class Form1 : Form
     {
+        List<Plants> plantList = new List<Plants>();
         public Form1()
         {
             InitializeComponent();
+            ShowInfo();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var plant = new Plants();
-            richTextBox1.Text = plant.GetInfo();
+            this.plantList.Clear();
+            var rnd = new Random();
+            for (var i = 0; i < 10; ++i)
+            {
+                switch (rnd.Next() % 3)
+                {
+                    case 0: 
+                        this.plantList.Add(new Flowers());
+                        break;
+                    case 1: 
+                        this.plantList.Add(new Bush());
+                        break;
+                    case 2: 
+                        this.plantList.Add(new Tree());
+                        break;
+                }
+            }
+            ShowInfo();
+        }
+        private void ShowInfo()
+        {
+            int flowersCount = 0;
+            int bushCount = 0;
+            int treeCount = 0;
+
+            foreach (var plant in this.plantList)
+            {
+                if (plant is Flowers) 
+                {
+                    flowersCount++;
+                }
+                else if (plant is Bush)
+                {
+                    bushCount++;
+                }
+                else if (plant is Tree)
+                {
+                    treeCount++;
+                }
+            }
+            richTextBox2.Text = "Цветок\tКуст\tДерево";
+            richTextBox2.Text += "\n";
+            richTextBox2.Text += String.Format("{0}\t{1}\t{2}", flowersCount, bushCount, treeCount);
         }
 
         private void button2_Click(object sender, EventArgs e)
