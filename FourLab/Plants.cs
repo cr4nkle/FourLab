cff7ed00267d195f861d21163c2e5a91f7c25347
Сергей Попович <cr4nkle@yandex.ru>
2021-10-31 16:06:30 +0800

    Добавил классы

---
+++ FourLab/Plants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourLab
+{ 
+    public class Plants
+    {
+
+    }
+
+    public class Flowers : Plants
+    {
+
+    }
+
+    public class Bush : Plants
+    {
+
+    }
+
+    public class Tree : Plants
+    {
+
+    }
+}
